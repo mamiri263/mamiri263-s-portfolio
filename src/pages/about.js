b63345b823686f5de0deffe2c4803bd5f7eeb1d2
mo <moh.85.amiri@gmail.com>
2022-10-24 02:11:01 +0300

    Finished making the base of the website

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+// Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+
+// Define your component
+const AboutPage = () => {
+  return (
+    <main>
+    <Layout pageTitle="About Me">
+      <p>Hi, I'm mamiri263 I enjoy playing osu and watching movies mostly!</p>
+    </Layout>
+    </main>
+  )
+}
+
+
+export const Head = () => ( 
+<>
+<meta name="description" content="Your description" /> 
+<title>About Me</title>
+</>
+)
+
+// Export your component
+export default AboutPage
