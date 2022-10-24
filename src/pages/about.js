2743f05c1b9a1436fda97fe24117889f7bbe9ace
mo <moh.85.amiri@gmail.com>
2022-10-24 03:11:39 +0300

    fixed code layouts + dark mode issue in 404

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,19 +7,19 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <main>
-    <Layout pageTitle="About Me">
-      <p>Hi, I'm mamiri263 I enjoy playing osu and watching movies mostly!</p>
-    </Layout>
+      <Layout pageTitle="About Me">
+        <p>Hi, I'm mamiri263 I enjoy playing osu and watching movies mostly!</p>
+      </Layout>
     </main>
   )
 }
 
 
-export const Head = () => ( 
-<>
-<meta name="description" content="Your description" /> 
-<title>About Me</title>
-</>
+export const Head = () => (
+  <>
+    <meta name="description" content="Your description" />
+    <title>About Me</title>
+  </>
 )
 
 // Export your component
