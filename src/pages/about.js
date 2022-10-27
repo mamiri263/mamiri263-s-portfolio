4a4d8eba7f96f833993cbf0e5476d12edd438491
mo <mo7ammadamiri@gmail.com>
2022-10-28 01:18:34 +0300

    added a single : in about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,7 +18,7 @@ const AboutPage = () => {
       <Layout pageTitle="About Me">
         <h1>Hi, I'm mamiri263</h1>
         <p>I'm your avarage highschool student who just has an interest in computer development.</p>
-        <h2>Contact me</h2>
+        <h2>Contact me:</h2>
         <ul>
           <li>discord: wyn#2006</li>
           <li><a style={linkStyles} href='mailto: mo7ammadamiri@gmail.com"'>email me</a></li>
