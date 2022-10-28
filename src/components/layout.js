44c01278e4c8c943407d8dd748c5f74e6f6800d5
mo <mo7ammadamiri@gmail.com>
2022-10-28 18:35:28 +0300

    might have fixed page links

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,17 +27,17 @@ const Layout = ({ pageTitle, children }) => {
         <nav>
           <ul className={navLinks}>
             <li className={navLinkItem}>
-              <Link to="/" className={navLinkText}>
+              <Link to="../index" className={navLinkText}>
                 Home
               </Link>
             </li>
             <li className={navLinkItem}>
-              <Link to="/about" className={navLinkText}>
+              <Link to="../about" className={navLinkText}>
                 About
               </Link>
             </li>
             <li className={navLinkItem}>
-              <Link to="/blog" className={navLinkText}>
+              <Link to="../blog" className={navLinkText}>
                 Blog
               </Link>
             </li>
