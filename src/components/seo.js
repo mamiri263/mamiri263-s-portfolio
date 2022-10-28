928e2b0ef0b58c52d1ffe7b8b0c1549e15051cb7
mo <mo7ammadamiri@gmail.com>
2022-10-28 23:00:31 +0300

    undo everything from last commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,30 +1,20 @@
-import React from "react"
-import { useSiteMetadata } from "../hooks/use-site-metadata"
+import * as React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
-export const SEO = ({ title, description, pathname, children }) => {
-  const { title: defaultTitle, description: defaultDescription, image, siteUrl, twitterUsername } = useSiteMetadata()
-
-  const Seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${siteUrl}${image}`,
-    url: `${siteUrl}${pathname || ``}`,
-    twitterUsername,
-  }
+const Seo = ({ title }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
   return (
-    <>
-      <title>{Seo.title}</title>
-      <meta name="description" content={Seo.description} />
-      <meta name="image" content={Seo.image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={Seo.title} />
-      <meta name="twitter:url" content={Seo.url} />
-      <meta name="twitter:description" content={Seo.description} />
-      <meta name="twitter:image" content={Seo.image} />
-      <meta name="twitter:creator" content={Seo.twitterUsername} />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
-      {children}
-    </>
+    <title>{title} | {data.site.siteMetadata.title}</title>
   )
 }
+
+export default Seo
