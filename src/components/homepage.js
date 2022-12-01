bb89cb930f049acd4c8daa65564df939ab619bfc
mo <mo7ammadamiri@gmail.com>
2022-12-02 01:42:44 +0300

    missed an unsaved file

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -16,7 +16,7 @@ const Homepage = () => {
         </div>
         <div className={aboutInfo}>
             <h1>Hello I'm <a target={"_blank"} style={{color: "gray"}}  href='https://github.com/mamiri263'>M</a></h1>
-            <p>I'm studying as a freshman in Kuwait and I have an interest in computer science.<br></br>I like stuff like cats, music and messing with my friends.</p>
+            <p>I'm studying as a freshman in Kuwait and I have an interest in computer science.<br></br>I like things such as cats, music and messing with my friends.</p>
             <p>I play games like <a style={{color: "gray"}} target="_blank" href='https://osu.ppy.sh/users/31195394'>osu</a> and minecraft and I read manga.</p>
             <h2 id='contacts'>contacts</h2>
             <p>You may contact me on discord: mamiri#1337 or email me <a style={{color: "gray"}} href='mailto: moh@mamiri263.com"'>here</a>.</p>
